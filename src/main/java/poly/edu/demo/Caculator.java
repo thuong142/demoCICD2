3acbc9576ec0c77db192dbf08a5f31aa2f290f4b
thuong142 <heyyouu1213@gmail.com>
2025-04-14 02:04:20 +0700

    test fail nha

--- src/main/java/poly/edu/demo/Caculator.java
+++ src/main/java/poly/edu/demo/Caculator.java
@@ -3,7 +3,7 @@ package poly.edu.demo;
 
 public class Caculator implements Icaculator{
 	public Integer sum(Integer a, Integer b) {
-		return a/b;
+		return 0;
 	}
 	
 	public Integer sub(Integer a, Integer b) {
