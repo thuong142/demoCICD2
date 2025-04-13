526b4259a6a53fcd51e07d9211d919c4aa0863d0
thuong142 <heyyouu1213@gmail.com>
2025-04-14 00:56:32 +0700

    first commit

---
+++ src/main/java/poly/edu/Lab8lan2KtApplication.java
@@ -0,0 +1,13 @@
+package poly.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab8lan2KtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab8lan2KtApplication.class, args);
+	}
+
+}
---
+++ src/main/java/poly/edu/demo/Icaculator.java
@@ -0,0 +1,7 @@
+package poly.edu.demo;
+
+public interface Icaculator {
+Integer sum(Integer a, Integer b);
+	
+	Integer sub(Integer a, Integer b);
+}
